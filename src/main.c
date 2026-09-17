761b1d932ef711b9b087228c0602dce53229861f
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-17 21:47:53 +0545

    doing smthhh

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void the_loop() {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("Slangish.Sh > ");
+        status = false;
+    } while (status);
+}
+
+int main() {
+    printf("Heyyy! Welcome to slangish-shell \n\n\n\n");
+    the_loop();
+    return EXIT_SUCCESS;
+}
